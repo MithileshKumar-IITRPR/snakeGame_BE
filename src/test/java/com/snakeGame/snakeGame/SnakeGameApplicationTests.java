1ebec9831b0ff02fcfb5dfbead5eb0d56a595dd4
Mithilesh Kumar <69746545+MithileshKumar-IITRPR@users.noreply.github.com>
2025-03-16 16:05:09 +0530

    Initial commit

---
+++ src/test/java/com/snakeGame/snakeGame/SnakeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snakeGame.snakeGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
